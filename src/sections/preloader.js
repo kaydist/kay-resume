b755b65186db3f6d3a1f624f71a99ae4f2bbadcd
kay <kayodeoseni@ymail.com>
2022-05-22 14:17:30 +0100

    Fixed preloader

--- src/sections/preloader.js
+++ src/sections/preloader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Preloader() {
   return (
-    <div className="h-screen w-screen fixed bg-[#111111] center z-50 preloader -translate-y-[100vh]">
+    <div className="h-screen w-screen fixed bg-[#111111] center z-50 preloader">
       <div className="overflow-hidden">
         <h2 className="text-white preloader-text">OSENI'S RESUME</h2>
       </div>
