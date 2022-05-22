57f6591e12b51ef25ca2e23d073e782934de89c2
kay <kayodeoseni@ymail.com>
2022-05-22 14:22:03 +0100

    Added opacity to preloader

--- src/sections/preloader.js
+++ src/sections/preloader.js
@@ -4,7 +4,7 @@ function Preloader() {
   return (
     <div className="h-screen w-screen fixed bg-[#111111] center z-50 preloader">
       <div className="overflow-hidden">
-        <h2 className="text-white preloader-text">OSENI'S RESUME</h2>
+        <h2 className="text-white preloader-text opacity-0">OSENI'S RESUME</h2>
       </div>
     </div>
   );
